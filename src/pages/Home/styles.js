c9235103631344cf1f0514bf57a65e5b8d5cf424
Gabrielle De Castro Santos Catarino <54675970+catarinogabrielle@users.noreply.github.com>
2021-04-29 00:16:46 -0300

    3

--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -1,8 +1,8 @@
 import styled from 'styled-components/native';
 
 export const Background = styled.View`
-    flex: 1;
-    background-color: #131313;
+ flex:1;
+ background-color: #131313;
 `;
 
 export const Container = styled.View`
@@ -12,31 +12,40 @@ export const Container = styled.View`
 
 export const Nome = styled.Text`
     font-size: 19px;
-    color: #fff;
+    color: #FFF;
     font-style: italic;
 `;
 
 export const Saldo = styled.Text`
-    margin-top: 5px;
-    font-size: 30px;
-    color: #fff;
-    font-weight: bold;
+margin-top: 5px;
+font-size: 30px;
+color: #FFF;
+font-weight: bold;
 `;
 
 export const Title = styled.Text`
-    margin-left: 15px;
-    color: #00b94a;
-    margin-bottom: 10px;
+margin-left: 5px;
+color: #00b94a;
+margin-bottom: 10px;
+
+`;
+
+export const Area = styled.View`
+flex-direction: row;
+margin-left: 15px;
+align-items: baseline;
 `;
 
 export const List = styled.FlatList.attrs({
     marginHorizontal: 15
 })`
     padding-top: 15px;
-    background-color: #fff;
+    background-color: #FFF;
     border-top-left-radius: 15px;
     border-top-right-radius: 15px;
     margin-left: 8px;
     margin-right: 8px;
+
 `;
+
 
--- src/pages/New/index.js
+++ src/pages/New/index.js
@@ -48,7 +48,7 @@ export default function New() {
     await firebase.database().ref('historico').child(uid).child(key).set({
       tipo: tipo,
       valor: parseFloat(valor),
-      date: format(new Date(), 'dd/MM/yy')
+      date: format(new Date(), 'dd/MM/yyyy')
     })
 
     //Atualizar o nosso saldo
